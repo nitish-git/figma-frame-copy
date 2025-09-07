021a36be80ce945e3263d399d8e5bf79a2e91e75
Nitish Kumar <pin.nitish@gmail.com>
2025-09-08 03:38:49 +0530

    duplicate card

--- src/app/v2/page.tsx
+++ src/app/v2/page.tsx
@@ -7,6 +7,13 @@ import { figmaFrames } from "./_frames";
 function Page() {
   return (
     <div className="grid grid-cols-3 gap-4 m-4">
+      <div className="group rounded aspect-square border border-gray-300 flex items-center justify-center bg-[url(/FigmaFrame3.png)] bg-contain bg-no-repeat bg-center">
+        <div className="flex flex-col items-center justify-center hover:bg-gray-300/70 w-full h-full">
+          <div className="hidden group-hover:flex">
+            <FigmaFrameCopierButtonV2 frame={figmaFrames[2]} />
+          </div>
+        </div>
+      </div>
       <div className="group rounded aspect-square border border-gray-300 flex items-center justify-center bg-[url(/FigmaFrame1.png)] bg-contain bg-no-repeat bg-center">
         <div className="flex flex-col items-center justify-center hover:bg-gray-300/70 w-full h-full">
           <div className="hidden group-hover:flex">
