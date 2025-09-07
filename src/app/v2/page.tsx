0bb4ad955624b9d05ca64aa81c46c57ff72cd978
Nitish Kumar <pin.nitish@gmail.com>
2025-09-07 20:02:39 +0530

    Add text

--- src/app/v2/page.tsx
+++ src/app/v2/page.tsx
@@ -7,6 +7,7 @@ import { figmaFrames } from "./_frames";
 function Page() {
   return (
     <div className="grid grid-cols-3 gap-4 m-4">
+      <h1>Frame</h1>
       <div className="group rounded aspect-square border border-gray-300 flex items-center justify-center bg-[url(/FigmaFrame1.png)] bg-contain bg-no-repeat bg-center">
         <div className="flex flex-col items-center justify-center hover:bg-gray-300/70 w-full h-full">
           <div className="hidden group-hover:flex">
